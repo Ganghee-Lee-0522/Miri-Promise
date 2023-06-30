2074fee5147a3b713ed26d0bc94eb2e79e4dd757
julia-heo <100216331+julia-heo@users.noreply.github.com>
2023-07-01 00:30:38 +0900

    MEDILIST 도메인 작성

---
+++ src/main/java/com/hackathon/cotato/hackathon/medication/domain/MediList.java
@@ -0,0 +1,33 @@
+package com.hackathon.cotato.hackathon.medication.domain;
+
+import com.hackathon.cotato.hackathon.member.domain.Member;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "TABLE_MEDILIST")
+public class MediList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false,name = "medilist_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(nullable = false,name = "member_id")
+    private Member member;
+
+    @Column(nullable = false,name = "medilist_name")
+    private String drug;
+
+    @Column(nullable = false,name = "medilist_time")
+    private String time;
+
+}
