b7233e4ac786438f8d9ae8aa6b3ee58d0169cc00
Ganghee Lee <gh_5226@naver.com>
2023-07-01 03:22:08 +0900

    feat: 날짜별 투약 정보 조회 개발 완료

--- src/main/java/com/hackathon/cotato/hackathon/medication/controller/MediTodayController.java
+++ src/main/java/com/hackathon/cotato/hackathon/medication/controller/MediTodayController.java
@@ -5,10 +5,7 @@ import com.hackathon.cotato.hackathon.medication.service.MediTodayService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -22,7 +19,16 @@ public class MediTodayController {
         try{
             return ResponseEntity.ok().body(ResponseDto.response(HttpStatus.OK, "Medication information returned successfully.", mediTodayService.getMediTodayInfo(phone)));
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body(ResponseDto.response(HttpStatus.BAD_REQUEST, "No registered medication information."));
+            return ResponseEntity.badRequest().body(ResponseDto.response(HttpStatus.BAD_REQUEST, e.getMessage()));
+        }
+    }
+
+    @GetMapping("/{date}")
+    public ResponseEntity<ResponseDto<Object>> getMediSpecialday(@RequestParam String date, @RequestBody String phone) {
+        try{
+            return ResponseEntity.ok().body(ResponseDto.response(HttpStatus.OK, "Medication information returned successfully.", mediTodayService.getMediSpecialdayInfo(date, phone)));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(ResponseDto.response(HttpStatus.BAD_REQUEST, e.getMessage()));
         }
     }
 }
--- src/main/java/com/hackathon/cotato/hackathon/medication/service/MediTodayService.java
+++ src/main/java/com/hackathon/cotato/hackathon/medication/service/MediTodayService.java
@@ -1,8 +1,6 @@
 package com.hackathon.cotato.hackathon.medication.service;
 
-import com.hackathon.cotato.hackathon.medication.domain.MediListFormat;
-import com.hackathon.cotato.hackathon.medication.domain.Medication;
-import com.hackathon.cotato.hackathon.medication.domain.MedicationRepository;
+import com.hackathon.cotato.hackathon.medication.domain.*;
 import com.hackathon.cotato.hackathon.medication.dto.TodayInfoRequestDto;
 import com.hackathon.cotato.hackathon.member.domain.Member;
 import com.hackathon.cotato.hackathon.member.domain.MemberRepository;
@@ -24,6 +22,8 @@ public class MediTodayService {
     private final MemberRepository memberRepository;
     private final MedicationRepository medicationRepository;
 
+    private final MediListRepository mediListRepository;
+
     public TodayInfoRequestDto getMediTodayInfo(String phone) {
         try {
             Optional<Member> member = memberRepository.findByPhone(phone);
@@ -31,22 +31,87 @@ public class MediTodayService {
                 throw new Exception("Account not found.");
             }
             else {
-                List<Medication> medi = medicationRepository.findByMember(member);
+                List<Medication> medis = medicationRepository.findByMember(member);
                 List<MediListFormat> mediInfoList = new ArrayList<>();
 
                 // 오늘 날짜 구하기
                 LocalDate now = LocalDate.now();
                 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
                 String formatedNow = now.format(formatter);
+                int cnt = 0;
 
-                for(Medication m : medi) {
+                for(Medication m : medis) {
                     if(m.getDate().equals(formatedNow)) {
                         String medi_name = m.getDrug();
                         String medi_time = m.getTime();
                         Boolean is_medi = m.isMedi();
                         MediListFormat mediListFormat = new MediListFormat(medi_name, medi_time, is_medi);
                         mediInfoList.add(mediListFormat);
+                        cnt++;
                     }
+                }
+                if(cnt == 0) {
+                    // mediListRepository를 조회한다
+                    List<MediList> mediLists = mediListRepository.findByMember(member);
+                    // 만약에 List가 null이면 error 반환(해당일자 X)
+                    if(mediLists.isEmpty()) {
+                        throw new Exception("No registered medication information.");
+                    }
+                    // List에 값이 있으면 List에 있는 애들을 하나씩 오늘 날짜로 저장하고, 이거를 다시 리스트로 만들어서 반환한다
+                    else {
+                        for(MediList ml : mediLists) {
+                            Medication medication=Medication.builder()
+                                    .drug(ml.getDrug())
+                                    .date(formatedNow)
+                                    .isMedi(false)
+                                    .member(member.get())//get써야 객체가 들어간다
+                                    .build();
+                            medicationRepository.save(medication);
+                        }
+                        List<Medication> newmedis = medicationRepository.findByMember(member);
+
+                        for(Medication m : newmedis) {
+                            if(m.getDate().equals(formatedNow)) {
+                                String medi_name = m.getDrug();
+                                String medi_time = m.getTime();
+                                Boolean is_medi = m.isMedi();
+                                MediListFormat mediListFormat = new MediListFormat(medi_name, medi_time, is_medi);
+                                mediInfoList.add(mediListFormat);
+                            }
+                        }
+                    }
+                }
+                return new TodayInfoRequestDto(mediInfoList);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    public TodayInfoRequestDto getMediSpecialdayInfo(String date, String phone) {
+        try {
+            Optional<Member> member = memberRepository.findByPhone(phone);
+            if(member.isEmpty()) {
+                throw new Exception("Account not found.");
+            }
+            else {
+                List<Medication> medis = medicationRepository.findByMember(member);
+                List<MediListFormat> mediInfoList = new ArrayList<>();
+
+                int cnt = 0;
+
+                for(Medication m : medis) {
+                    if(m.getDate().equals(date)) {
+                        String medi_name = m.getDrug();
+                        String medi_time = m.getTime();
+                        Boolean is_medi = m.isMedi();
+                        MediListFormat mediListFormat = new MediListFormat(medi_name, medi_time, is_medi);
+                        mediInfoList.add(mediListFormat);
+                        cnt++;
+                    }
+                }
+                if(cnt == 0) {
+                    throw new Exception("No medication history for that date.");
                 }
                 return new TodayInfoRequestDto(mediInfoList);
             }
