fe6adb0effb5ea4377dd88d19c78e9403155e1bb
julia-heo <100216331+julia-heo@users.noreply.github.com>
2023-07-01 00:59:32 +0900

    mediList 폴더 생성

---
+++ src/main/java/com/hackathon/cotato/hackathon/medication/controller/MakeMedListController.java
@@ -0,0 +1,15 @@
+package com.hackathon.cotato.hackathon.medication.controller;
+
+import com.hackathon.cotato.hackathon.medication.service.NewMediService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+
+public class MakeMedListController {
+    private final NewMediService newMediService;
+
+    @PostMapping("/miri-promise/medi/new")
+    public ResponseEntity
+
+
+
+}
---
+++ src/main/java/com/hackathon/cotato/hackathon/medication/service/NewMediService.java
@@ -0,0 +1,4 @@
+package com.hackathon.cotato.hackathon.medication.service;
+
+public class NewMediService {
+}
