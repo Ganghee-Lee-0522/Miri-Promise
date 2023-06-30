37e39baab5e8b87feb3f5735e00ccb154a05284f
Ganghee Lee <gh_5226@naver.com>
2023-07-01 02:10:55 +0900

    fix: 왜 안될까요..

---
+++ src/main/java/com/hackathon/cotato/hackathon/medication/domain/MedicationRepository.java
@@ -0,0 +1,11 @@
+package com.hackathon.cotato.hackathon.medication.domain;
+
+import com.hackathon.cotato.hackathon.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MedicationRepository extends JpaRepository<Medication, Long> {
+    List<Medication> findByMember(Optional<Member> member);
+}
